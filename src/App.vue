5df5f699fd3d987414dc46bdf74575e4387e0c74
Hannasezen <andreeva0649@gmail.com>
2018-11-12 18:19:23 +0200

    animation

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="training">
     <h1>Math training</h1>
-    <AppStartScreen
-      v-if="state === 'start'"
-      @onStart="onStart"
-      ></AppStartScreen>
-    <AppQuestion
-      v-else-if="state === 'question'"
-      @success="onSuccess"
-      @error="onError"
-    ></AppQuestion>
-    <AppMessage
-      v-else-if="state === 'message'"
-      :type="message.type"
-      :text="message.text"
-      @onNext="onNext"
-      ></AppMessage>
-    <AppResultScreen v-else-if="state === 'result'"></AppResultScreen>
-    <div v-else>Unknown state</div>
+    <div class="progress">
+      <div class="progress-bar"></div>
+    </div>
+    <div class="box">
+      <transition name="flip" mode="out-in">
+        <AppStartScreen
+            v-if="state === 'start'"
+            @onStart="onStart"
+        ></AppStartScreen>
+        <AppQuestion
+            v-else-if="state === 'question'"
+          @success="onSuccess"
+            @error="onError"
+        ></AppQuestion>
+        <AppMessage
+          v-else-if="state === 'message'"
+          :type="message.type"
+          :text="message.text"
+          @onNext="onNext"
+        ></AppMessage>
+        <AppResultScreen v-else-if="state === 'result'"></AppResultScreen>
+        <div v-else>Unknown state</div>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -30,12 +37,22 @@ export default {
       message: {
         type: '',
         text: ''
-      }
+      },
+      stats: {
+        success: 0,
+        error: 0
+      },
+      questMax: 3
+    }
+  },
+  computed: {
+    questDone() {
+      return this.stats.success + this.stats.error;
     }
   },
   methods: {
     onStart(){
-      this.state = 'question'
+      this.state = 'question';
     },
     onSuccess() {
       this.state = 'message';
@@ -58,6 +75,36 @@ export default {
 .training {
   max-width: 700px;
   margin: 20px auto;
-  ;
 }
+
+.box {
+  margin: 10px 0;
+}
+
+.flip-enter {
+
+}
+
+.flip-enter-active {
+  animation: flipInX 0.3s linear;
+}
+
+.flip-leave {
+
+}
+
+.flip-leave-active {
+  animation: flipOutX 0.3s linear;
+}
+
+@keyframes flipInX {
+  from{transform: rotateX(90deg);}
+  to{transform: rotateX(0deg)}
+}
+
+@keyframes flipOutX {
+  from{transform: rotateX(0deg);}
+  to{transform: rotateX(90deg)}
+}
+
 </style>
